193079c44a0364322c6d93928faf50467cd31b58
Stian Soiland-Reyes <stain@apache.org>
2018-05-17 11:24:59 +0100

    hard-coded doi workarounds
    run html tidy, fall back to cp
    
    fetch crossref metadata as JSON

--- scripts/process.sh
+++ scripts/process.sh
@@ -15,16 +15,52 @@ for html in $(find ../data/delivery.acm.org/ -name '*html' -type f) ; do
   echo "---"
   echo "Processing $html"
 
-  doi=$(sed -n "s,.*[\"']https://doi.org/\([^\"']*\).*,\1,p;q" $html)
+  # assume a self-DOI at the very bottom under ACM's 10.1145/ tree
+  # and a sub-id of 123.456 or 123
 
+  name=$(basename $html)
+  case "$name" in 
+    # Hard-coded exceptions as they for whatever reason won't match
+    # regex below
+    "p1749-thompson.html") doi="10.1145/3184558.3191636";;
+    "p753-youngmann.html") doi="10.1145/3178876.3186156";;
+    "p721-veen.html") doi="10.1145/3184558.3185978";;
+    "p409-yoon.html") doi="10.1145/3178876.3186107";;    
+    *) 
+      # The rest should match this pattern
+      doi=$(sed -n "s,.*[\"']https://doi.org/\(10.1145\/[0-9.]*\).*,\1,p;q" $html)
+  esac
+
+  if [[ -z $doi ]] ; then
+    echo "Can't find DOI of $html" >&2;
+    exit;
+  fi
+  
   echo "doi: $doi"
-  dest="$pathToDoi"/$doi/index.html
-  echo "Saving to $dest"
-  mkdir -p $(dirname "$dest")
-  cp  $html $dest
-  # -file $pathToLog/something.txt
-  #tidy --drop-empty-elements no -indent $html > $dest || true
 
+  echo Fetching crossref metadata for $doi
+  json="$pathToDoi"/$doi/crossref.json
+  wget -q -O $json https://api.crossref.org/v1/works/http://dx.doi.org/$doi &
+
+
+  dest="$pathToDoi"/$doi/index.html
+  echo "Saving HTML to $dest"
+  mkdir -p $(dirname "$dest")
+
+  logfile="$pathToLog"/$(echo $doi | sed s,/,_,g).tidy.log
+  echo "Tidying HTML"
+  #Make sure you install from https://github.com/htacg/tidy-html5
+
+  tidy -quiet -file "$logfile" --drop-empty-elements no -indent $html > $dest || (
+    # Exit code 1 is just warnings, which we can ignore
+    if [ $? -ne 1 ]; then 
+      echo "tidy failed, copying HTML as-is" >$logfile
+      echo "tidy failed, copying HTML as-is" >&2
+      cp $html $dest
+    fi;
+  )
+
+  wait
   filename=$(echo "$html" | sed -r 's#&#&amp;#g')
   echo "Fixing image links"
   sed -i 's#http://deliveryimages.acm.org/#../../../data/deliveryimages.acm.org/#g' "$dest"
